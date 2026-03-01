ea50b94891cf0330acd4d3c7141b9d977169b90f
vinz516 <inivinz12@gmail.com>
2026-03-01 11:46:37 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,14 @@ import Script from 'next/script'
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* Histats Tracking */}
+      {/* Histats Tracking Baru - ID: 5011680 */}
       <Script 
         id="histats-script" 
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             var _Hasync= _Hasync|| [];
-            _Hasync.push(['Histats.start', '1,5011326,4,0,0,0,00010000']);
+            _Hasync.push(['Histats.start', '1,5011680,4,0,0,0,00010000']);
             _Hasync.push(['Histats.fasi', '1']);
             _Hasync.push(['Histats.track_hits', '']);
             (function() {
@@ -21,6 +21,14 @@ export default function MyApp({ Component, pageProps }) {
           `,
         }}
       />
+      
+      {/* Noscript Fallback */}
+      <noscript>
+        <a href="/" target="_blank">
+          <img src="//sstatic1.histats.com/0.gif?5011680&101" alt="histats" border="0" />
+        </a>
+      </noscript>
+
       <Component {...pageProps} />
     </>
   )
